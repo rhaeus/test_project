e5d6ec4b284b2c536c1d9cb56199a60b51deea33
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2023-03-07 11:43:14 +0100

    make test fail

--- tests/main.cpp
+++ tests/main.cpp
@@ -12,5 +12,6 @@ TEST_CASE("Factorials are computed", "[factorial]") {
   REQUIRE(factorial(1) == 1);
   REQUIRE(factorial(2) == 2);
   REQUIRE(factorial(3) == 6);
+  REQUIRE(factorial(3) == 7);
   REQUIRE(factorial(10) == 3'628'800);
 }
