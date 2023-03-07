102bdb60173f199f00f538c4262417cf2c2393c5
Ramona Häuselmann <ramona.haeuselmann@gmail.com>
2023-03-07 11:44:40 +0100

    ctest

--- tests/main.cpp
+++ tests/main.cpp
@@ -12,6 +12,5 @@ TEST_CASE("Factorials are computed", "[factorial]") {
   REQUIRE(factorial(1) == 1);
   REQUIRE(factorial(2) == 2);
   REQUIRE(factorial(3) == 6);
-  REQUIRE(factorial(3) == 7);
   REQUIRE(factorial(10) == 3'628'800);
 }
